5f104c2e645d06ae270e2c959d48dc4d8fb08703
DESKTOP-N0OL9O7\YOO TAE WOOK <xodnr106@naver.com>
2023-05-04 16:31:17 +0900

    [Maple_api] 현재날짜까지 데이터 추출 기능 추가

--- Maple_sample_api.py
+++ Maple_sample_api.py
@@ -1,40 +1,55 @@
 import requests, json
+from datetime import timedelta, datetime
 
-with open("./resource/API_key.json", 'rt') as fp1:
-    value_result = fp1.read()
+def date_range(start, end):
+    start = datetime.strptime(start, "%Y-%m-%d")
+    end = datetime.strptime(end, "%Y-%m-%d")
+    dates = [(start + timedelta(days=i)).strftime("%Y-%m-%d") for i in range((end-start).days+1)]
+    return dates
 
-auth_key = json.loads(value_result).get('Authorization', {})
-header = {"Authorization":auth_key}
-count = 1000
-date = "2023-05-02"
 
-url = f"https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results?count={count}&date={date}"#&cursor={cursor}"
 
-result = requests.get(url, headers=header)
-print(result.text)
-data_dict = json.loads(result.text)
-print(data_dict)
-black_count = 0
-add_cube_count = 0
-black_sucess = 0
-black_fail = 0
-add_cube_sucess = 0
-add_cube_fail = 0
-for i in reversed(data_dict.get('cube_histories', {})):
-    if i.get('character_name') == '오라웨푼':
-        if i.get('cube_type') == '블랙 큐브':
-            black_count += 1
-            if i.get('item_upgrade_result') == '성공':
-                black_sucess += 1
-                black_count_sucess = black_count
-            else:
-                black_fail += 1
-        elif i.get('cube_type') == '에디셔널 큐브':
-            add_cube_count += 1
-            if i.get('item_upgrade_result') == '성공':
-                add_cube_sucess += 1
-                add_cube_count_sucess = add_cube_count
-            else:
-                add_cube_fail += 1
+def main():
+    with open("./resource/API_key.json", 'rt') as fp1:
+        value_result = fp1.read()
+    auth_key = json.loads(value_result).get('Authorization', {})
+    header = {"Authorization": auth_key}
+    count = 1000
+    date = "2022-11-25"
+    date = date_range(date, datetime.strftime(datetime.now(), "%Y-%m-%d"))
+    print(date)
+    for cube_date in date:
+        url = f"https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results?count={count}&date={cube_date}"#&cursor={cursor}"
+        result = requests.get(url, headers=header)
+        print(result.text)
+        data_dict = json.loads(result.text)
+        print(data_dict)
+        black_count = 0
+        black_count_sucess = 0
+        add_cube_count_sucess = 0
+        add_cube_count = 0
+        black_sucess = 0
+        black_fail = 0
+        add_cube_sucess = 0
+        add_cube_fail = 0
+        for i in reversed(data_dict.get('cube_histories', {})):
+            if i.get('character_name') == '오라웨푼':
+                if i.get('cube_type') == '블랙 큐브':
+                    black_count += 1
+                    if i.get('item_upgrade_result') == '성공':
+                        black_sucess += 1
+                        black_count_sucess = black_count
+                    else:
+                        black_fail += 1
+                elif i.get('cube_type') == '에디셔널 큐브':
+                    add_cube_count += 1
+                    if i.get('item_upgrade_result') == '성공':
+                        add_cube_sucess += 1
+                        add_cube_count_sucess = add_cube_count
+                    else:
+                        add_cube_fail += 1
 
-print(black_count_sucess, black_sucess, black_fail, add_cube_count_sucess, add_cube_sucess, add_cube_fail)
+        print(black_count_sucess, black_sucess, black_fail, add_cube_count_sucess, add_cube_sucess, add_cube_fail)
+
+if __name__ == '__main__':
+    main()
